df126a8bd8d1de88c9d0f7d4783b422e0908cd5e
Chris Gonzales <cm.gonzales@yahoo.com>
2017-10-02 20:20:15 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+    $("h1").fadeIn(8300);
+    $(".intro-message").animate({left: "320px"}, 10000);
+
+$(".navbar-brand").click(function(){
+    $("h1").fadeIn(8300);
+    $(".intro-message").animate({left: "320px"}, 10000);
+});
+});
