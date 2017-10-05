cb6cf85d982043143eeadc5710ec4819fc00f328
Chris Gonzales <cm.gonzales@yahoo.com>
2017-10-05 16:45:16 -0700

    added final

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
     $("h1").fadeIn(8300);
-    $(".intro-message").animate({left: "320px"}, 10000);
-   
-});
+    $(".intro-message").animate({left: "25%"}, 10000);
 
- 
+ });
